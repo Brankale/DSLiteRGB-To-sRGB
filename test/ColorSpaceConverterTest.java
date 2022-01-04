6e6dd72e68e49cd9f7cdaff7bc80688b08acdb35
Brankale <45881310+Brankale@users.noreply.github.com>
2022-01-04 15:14:44 +0100

    added new tests

--- test/ColorSpaceConverterTest.java
+++ test/ColorSpaceConverterTest.java
@@ -1,65 +1,22 @@
 import colorspaces.DSLite;
 import colorspaces.SRGB;
 import converters.ColorSpaceConverter;
-import converters.OutsideGamutException;
 import coordinates.data_types.CIEXYZ;
 import org.ejml.simple.SimpleMatrix;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.awt.*;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class ColorSpaceConverterTest {
 
-    private static final double DELTA = 0.000001;
+    private static final double DELTA = 0.00001;
 
     public static SRGB srgb = new SRGB();
     public static DSLite dsLite = new DSLite();
-    public static ColorSpaceConverter dslToSRgbConverter = new ColorSpaceConverter(dsLite, srgb);
-    public static ColorSpaceConverter sRgbToDslConverter = new ColorSpaceConverter(srgb, dsLite);
-
-    @Test
-    public void dslBlackShouldBeEqualToSRgbBlack() {
-        assertEquals(Color.BLACK, dslToSRgbConverter.convert(Color.BLACK));
-    }
-
-    /**
-     * xy coords are inside sRGB gamut but Y should
-     * also be considered, but it's unknown
-     */
-    @Ignore
-    public void dslRedShouldBeInsideSRgbGamut() {
-        dslToSRgbConverter.convert(Color.RED);
-    }
-
-    /**
-     * xy coords are outside sRGB gamut ==> color is outside sRGB gamut
-     */
-    @Test
-    public void dslGreenShouldBeOutsideSRgbGamut() {
-        assertThrows(OutsideGamutException.class, () -> {
-            dslToSRgbConverter.convert(Color.GREEN);
-        });
-    }
-
-    /**
-     * xy coords are outside sRGB gamut ==> color is outside sRGB gamut
-     */
-    @Test
-    public void dslBlueShouldBeOutsideSRgbGamut() {
-        assertThrows(OutsideGamutException.class, () -> {
-            dslToSRgbConverter.convert(Color.BLUE);
-        });
-    }
-
-    @Test
-    public void sRgbRedShouldBeOutsideDslGamut() {
-        assertThrows(OutsideGamutException.class, () -> {
-            sRgbToDslConverter.convert(Color.RED);
-        });
-    }
+    public static ColorSpaceConverter dslToSRgbConverter = new ColorSpaceConverter(dsLite, srgb, false);
 
     @Test
     public void dslRedShouldBeConvertedToItsChromaticityValue() {
@@ -95,6 +52,62 @@ public class ColorSpaceConverterTest {
 
         assertEquals(ciexyz.toCIExyY().x, dsLite.w.x, DELTA);
         assertEquals(ciexyz.toCIExyY().y, dsLite.w.y, DELTA);
+    }
+
+    @Test
+    public void averageOfChannelGammaMustBeEqualToGreyscaleGamma() {
+        for (int i = 0; i <= 255; i += 4) {
+            double normalizedColor = i / 255.0;
+            double gammaRed = DSLite.getRedGamma(normalizedColor);
+            double gammaGreen = DSLite.getGreenGamma(normalizedColor);
+            double gammaBlue = DSLite.getBlueGamma(normalizedColor);
+            double gammaGrey = DSLite.getGreyGamma(normalizedColor);
+            assertEquals(gammaGrey, (gammaRed + gammaGreen + gammaBlue) / 3.0, DELTA);
+        }
+    }
+
+    @Test
+    public void dslBlackShouldBeEqualToSRgbBlack() {
+        assertEquals(Color.BLACK, dslToSRgbConverter.convert(Color.BLACK));
+    }
+
+    @Test
+    public void darkestRedMustBeDifferentFromBlackAfterConversion() {
+        Color color = new Color(4, 0, 0);
+        assertNotEquals(Color.BLACK, dslToSRgbConverter.convert(color));
+    }
+
+    // TODO: should be outside the gamut?
+    @Test
+    public void darkestGreenMustBeDifferentFromBlackAfterConversion() {
+        Color color = new Color(0, 4, 0);
+        assertNotEquals(Color.BLACK, dslToSRgbConverter.convert(color));
+    }
+
+    // TODO: should be outside the gamut?
+    @Test
+    public void darkestBlueMustBeDifferentFromBlackAfterConversion() {
+        Color color = new Color(0, 0, 4);
+        assertNotEquals(Color.BLACK, dslToSRgbConverter.convert(color));
+    }
+
+    // TODO: should be outside the gamut?
+    @Test
+    public void darkestCyanMustBeDifferentFromBlackAfterConversion() {
+        Color color = new Color(0, 4, 4);
+        assertNotEquals(Color.BLACK, dslToSRgbConverter.convert(color));
+    }
+
+    @Test
+    public void darkestMagentaMustBeDifferentFromBlackAfterConversion() {
+        Color color = new Color(4, 0, 4);
+        assertNotEquals(Color.BLACK, dslToSRgbConverter.convert(color));
+    }
+
+    @Test
+    public void darkestYellowMustBeDifferentFromBlackAfterConversion() {
+        Color color = new Color(4, 4, 0);
+        assertNotEquals(Color.BLACK, dslToSRgbConverter.convert(color));
     }
 
 }
